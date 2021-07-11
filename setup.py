97aa4ec69e167fa12baf843d60b269d61eb35cbc
Muhammed Ali <muhammed@heybooster.ai>
2021-07-11 04:51:07 +0300

    setup.py fixed

--- setup.py
+++ setup.py
@@ -8,6 +8,10 @@ from setuptools import setup
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
+requirements_filename = os.path.join(os.path.dirname(__file__), 'requirements.txt')
+with open(requirements_filename) as f:
+    PACKAGE_INSTALL_REQUIRES = [line[:-1] for line in f]
+
 setup(
     name = "trendyol_api_python_sdk",
     version = "0.0.1",
@@ -16,15 +20,14 @@ setup(
     description = ("Unofficial Trendyol API Python SDK"),
     license = "MIT",
     url="https://github.com/altuntasmuhammet/trendyol-api-python-sdk",
-    packages=[
-        'trendyol_sdk'
-        ],
-    long_description=read('README'),
+    packages=['trendyol_sdk'],
+    long_description=read('README.md'),
     long_description_content_type="text/markdown",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    install_requires=PACKAGE_INSTALL_REQUIRES,
     python_requires=">=3.0",
 )
